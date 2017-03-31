15da90a5c6b194fde8fa9a21e5e7e73e115e2469
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-03-31 10:16:37 +0100

    Feature test for player losing game

---
+++ spec/features/losing_spec.rb
@@ -0,0 +1,11 @@
+feature "Confirmation of player losing" do
+  scenario 'Player 2 loses with player 1 attacks' do
+    sign_in_and_play
+    9.times do
+      click_button "HIT!"
+      click_button 'OK'
+    end
+    expect(page).to have_content "You have been killed"
+
+  end
+end
